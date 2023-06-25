a7eca2f49a3159af93cd37e6dc5dc6fe63756bb5
Pachy <pachycat94@gmail.com>
2023-06-25 17:09:07 -0300

    Codigo base terminado

---
+++ javascript.js
@@ -0,0 +1,7 @@
+/* Cuando hago CLICK .button, .nav TOGGLE 'activo' */
+const button = document.querySelector('.button')
+const nav    = document.querySelector('.nav')
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('activo')
+})
